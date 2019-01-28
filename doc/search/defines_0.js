9e0446dafbbc6ad2a7fd476608b1eff76f0310f1
Bruno Morais <brunosmmm@gmail.com>
2019-01-28 14:52:13 -0500

    added more generated documentation

---
+++ doc/search/defines_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['zed_5foled_5fh',['ZED_OLED_H',['../zed__oled_8h.html#a3aef5d8d6a6963453ba61860e462ce73',1,'zed_oled.h']]],
+  ['zed_5foled_5fw',['ZED_OLED_W',['../zed__oled_8h.html#a8d3fe4536958efec1c5959e55748fed2',1,'zed_oled.h']]]
+];
---
+++ doc/search/functions_7.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['zedoled_5fall_5foff',['zedoled_all_off',['../zed__oled_8h.html#acc74a70bf15c5f1529aa4a33511d337a',1,'zed_oled.h']]],
+  ['zedoled_5fall_5fon',['zedoled_all_on',['../zed__oled_8h.html#a9f1df26b78dfd28e062c57ae43532886',1,'zed_oled.h']]],
+  ['zedoled_5fclear',['zedoled_clear',['../zed__oled_8h.html#a9c0bc7b369edd887e4e0a62269d1d593',1,'zed_oled.h']]],
+  ['zedoled_5ffill',['zedoled_fill',['../zed__oled_8h.html#adb84899e2cb800b7609a449804f858a8',1,'zed_oled.h']]],
+  ['zedoled_5ffree',['zedoled_free',['../zed__oled_8h.html#a82d9a5ab480272750646c8798cb7d3a8',1,'zed_oled.h']]],
+  ['zedoled_5fget',['zedoled_get',['../zed__oled_8h.html#af23f060fe5b7da72ad6a53c82a41654c',1,'zed_oled.h']]],
+  ['zedoled_5finitialize',['zedoled_initialize',['../zed__oled_8h.html#a564e969fc4c4cfa4ce3ab1455b3bfc8e',1,'zed_oled.h']]],
+  ['zedoled_5finverse',['zedoled_inverse',['../zed__oled_8h.html#a05bc1f03b2778282b27bdd1324848d15',1,'zed_oled.h']]],
+  ['zedoled_5fnormal',['zedoled_normal',['../zed__oled_8h.html#a5712cea99b9597fd9e3ca18df7efe653',1,'zed_oled.h']]],
+  ['zedoled_5fset_5fpixel',['zedoled_set_pixel',['../zed__oled_8h.html#a972d68230f34cc63cc6dd2a2858264a7',1,'zed_oled.h']]],
+  ['zedoled_5fupdate',['zedoled_update',['../zed__oled_8h.html#abd5b6ca264f08f864b38fad3084a8a2e',1,'zed_oled.h']]]
+];
