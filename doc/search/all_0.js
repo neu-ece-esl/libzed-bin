3e9bbc73d037312ca22aadf9b54b51cf8bc28acf
Bruno Morais <brunosmmm@gmail.com>
2019-01-28 12:42:34 -0500

    add generated documentation

---
+++ doc/search/all_0.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['axi_5fgpio_2eh',['axi_gpio.h',['../axi__gpio_8h.html',1,'']]],
+  ['axigpio_5fchannel_5fget',['axigpio_channel_get',['../axi__gpio_8h.html#aa74f694bc751a2efcf4a5c8d20329ea9',1,'axi_gpio.h']]],
+  ['axigpio_5fchannel_5fset',['axigpio_channel_set',['../axi__gpio_8h.html#a8ea9360651048f7d16c534ff994d2029',1,'axi_gpio.h']]],
+  ['axigpio_5ffree',['axigpio_free',['../axi__gpio_8h.html#a262a0cb3210c0042fae0c2c1e9e99d54',1,'axi_gpio.h']]],
+  ['axigpio_5fget',['axigpio_get',['../axi__gpio_8h.html#abf45efc0c532449b50ecd92f97345bd9',1,'axi_gpio.h']]],
+  ['axigpio_5fgroup_5fclear',['axigpio_group_clear',['../axi__gpio_8h.html#aa746ce7e321521ee6e6c5eb40a12636b',1,'axi_gpio.h']]],
+  ['axigpio_5fgroup_5finput',['axigpio_group_input',['../axi__gpio_8h.html#ab07c766331593608d41a55584909b624',1,'axi_gpio.h']]],
+  ['axigpio_5fgroup_5foutput',['axigpio_group_output',['../axi__gpio_8h.html#aecd6157f70b1a021ff447430cf819aaa',1,'axi_gpio.h']]],
+  ['axigpio_5fgroup_5fset',['axigpio_group_set',['../axi__gpio_8h.html#aeff2d8e9b445d5391463710bc9563f52',1,'axi_gpio.h']]],
+  ['axigpio_5fsingle_5fclear',['axigpio_single_clear',['../axi__gpio_8h.html#a179421867c036efaaca138514d66734f',1,'axi_gpio.h']]],
+  ['axigpio_5fsingle_5fget',['axigpio_single_get',['../axi__gpio_8h.html#ac0ff5fae692dcdf44ebeb91f6d839633',1,'axi_gpio.h']]],
+  ['axigpio_5fsingle_5finput',['axigpio_single_input',['../axi__gpio_8h.html#a49a8e2ca7948b700d223745a8ff1ee74',1,'axi_gpio.h']]],
+  ['axigpio_5fsingle_5foutput',['axigpio_single_output',['../axi__gpio_8h.html#a8a3b47a0987a98f852ad99780264dc75',1,'axi_gpio.h']]],
+  ['axigpio_5fsingle_5fset',['axigpio_single_set',['../axi__gpio_8h.html#a6c1d83b547463f8ae176b453ff01cf4d',1,'axi_gpio.h']]]
+];
---
+++ doc/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['axi_5fgpio_2eh',['axi_gpio.h',['../axi__gpio_8h.html',1,'']]]
+];
